e88550c325c795dcb5710129eab3a68f95558f70
Nikimizajeli <86631409+Nikimizajeli@users.noreply.github.com>
2023-01-20 23:29:39 +0100

    dodanie klasy UnionFind

--- 51631.algo2.lab08.main.cpp
+++ 51631.algo2.lab08.main.cpp
@@ -46,6 +46,79 @@ public:
 
 };
 
+class UnionFind {
+private:
+    int *indeksyRodzicow;
+    int *rangi;
+    int iloscWezlow;
+
+    int rodzic(int indeks) {
+        if (indeks < 0 || indeks >= iloscWezlow) {
+            throw std::invalid_argument("Indeks poza zakresem.");
+        }
+
+        return indeksyRodzicow[indeks];
+    }
+
+public:
+    explicit UnionFind(int iloscWezlow) : indeksyRodzicow(new int[iloscWezlow]), rangi(new int[iloscWezlow]),
+                                          iloscWezlow(iloscWezlow) {
+        for (int i = 0; i < iloscWezlow; i++) {
+            // utworzenie jednoelementowego zbioru dla kazdego wezla, gdzie reprezentantem jest on sam
+            indeksyRodzicow[i] = i;
+            rangi[i] = 0;
+        }
+    }
+
+    ~UnionFind() {
+        delete[] indeksyRodzicow;
+        delete[] rangi;
+    }
+
+    // b) laczenie dwoch zbiorow, argumenty dwa indeksy wezlow
+    // zrobic na dwa sposoby - zwykle oraz union-by-rank
+    void scalZbiory(int reprezentantA, int reprezentantB) {
+        if (reprezentantA == reprezentantB) {
+            throw std::invalid_argument("Zbiory nie sa rozlaczne.");
+        }
+
+        indeksyRodzicow[reprezentantB] = reprezentantA;
+    }
+
+    void scalZbioryUnionByRank(int reprezentantA, int reprezentantB) {
+        if (reprezentantA == reprezentantB) {
+            throw std::invalid_argument("Zbiory nie sa rozlaczne.");
+        }
+
+        if (rangi[reprezentantA] < rangi[reprezentantB]) {
+            indeksyRodzicow[reprezentantA] = reprezentantB;
+        } else if (rangi[reprezentantA] > rangi[reprezentantB]) {
+            indeksyRodzicow[reprezentantB] = reprezentantA;
+        } else {
+            indeksyRodzicow[reprezentantB] = reprezentantA;
+            rangi[reprezentantA]++;
+        }
+    }
+
+    // c) znajdowanie reprezentanta zbioru, arg indeks wezla, wynik indeks reprezentanta
+    // dwa sposoby - bez i z kompresja sciezki
+    int znajdzReprezentanta(int indeks) {
+        while (indeks != rodzic(indeks)) {
+            indeks = rodzic(indeks);
+        }
+
+        return indeks;
+    }
+
+    int znajdzReprezentantaPathCompression(int indeks) {
+        if (indeks != rodzic(indeks)) {
+            indeksyRodzicow[indeks] = znajdzReprezentantaPathCompression(rodzic(indeks));
+        }
+
+        return rodzic(indeks);
+    }
+};
+
 int main() {
 
     return 0;
